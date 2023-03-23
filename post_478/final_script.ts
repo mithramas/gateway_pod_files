0f43e7a5c22e5a4e0ed60a34eb7c54900ec13a5f
jcavaiuolo <j.cavaiuolo@gmail.com>
2023-03-23 16:56:42 -0300

    Create final_script.ts

---
+++ post_478/final_script.ts
@@ -0,0 +1,43 @@
+export default {
+  async fetch(request) {
+        const formData = await request.formData();
+        const name = formData.get('name');
+        const email = formData.get('email');
+        const textarea = formData.get('textarea');
+                const today = new Date();
+
+    let send_request = new Request('https://api.mailchannels.net/tx/v1/send', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        personalizations: [
+          {
+            to: [{ email: 'xxxxxxxx@zzzzzzzz.com', name: 'Test Recipient' }],
+          },
+        ],
+        from: {
+          email: 'yyyyyyyy@zzzzzzzz.com',
+          name: name,
+        },
+        subject: 'Solicitud de contacto recibida a traves del sitio',
+        content: [
+          {
+            type: 'text/plain',
+            value: 'Se ha recibido el sigiuiente mensaje a traves del sitio: \n\n' + textarea + '.\n\nDe parte de: ' + name +'.\n'+ email + '.\n\n\n\n' + today +'.',
+          },
+        ],
+      }),
+    })
+  const response = await fetch(send_request);
+        const responseHeaders = new Headers(response.headers)
+        responseHeaders.set('Access-Control-Allow-Origin', '*')
+        return new Response(response.body, {
+            headers: responseHeaders,
+            status: response.status,
+            statusText: response.statusText
+        })
+
+  },
+}
